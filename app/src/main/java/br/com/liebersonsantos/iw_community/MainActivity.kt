80253b426a56e8282eba4a9dd0bc6836cac13bd6
liebersonsantos <liebersonsantos.one@gmail.com>
2021-09-06 14:20:17 -0300

    Initial commit Settings

---
+++ app/src/main/java/br/com/liebersonsantos/iw_community/MainActivity.kt
@@ -0,0 +1,11 @@
+package br.com.liebersonsantos.iw_community
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
